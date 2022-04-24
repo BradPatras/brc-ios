50d0003cac13e43708b5bf541f10d14d6622f4a8
Brad <BradPatras@users.noreply.github.com>
2022-04-24 17:26:02 -0500

    Added demo project

---
+++ brc-ios-demo/brc-ios-demo/ContentView.swift
@@ -0,0 +1,47 @@
+//
+//  ContentView.swift
+//  brc-ios-demo
+//
+//  Created by Brad Patras on 4/24/22.
+//
+
+import BasicRemoteConfigs
+import SwiftUI
+
+struct ContentView: View {
+	let brc: BasicRemoteConfigs
+	@State var text: String = "Hello, world!"
+	
+    var body: some View {
+		VStack {
+			Text(text)
+				.padding()
+				.font(.caption)
+			
+			Button(action: {
+				Task {
+					do {
+						try await brc.fetchConfigs()
+						text = brc.values.prettyPrint()
+					} catch {
+						text = error.localizedDescription
+					}
+				}
+			}) {
+				Text("Fetch configs")
+			}
+		}
+    }
+}
+
+extension Dictionary where Key == String, Value == Any {
+	func prettyPrint() -> String{
+		var string: String = ""
+		if let data = try? JSONSerialization.data(withJSONObject: self, options: .prettyPrinted){
+			if let nstr = NSString(data: data, encoding: String.Encoding.utf8.rawValue){
+				string = nstr as String
+			}
+		}
+		return string
+	}
+}
---
+++ brc-ios-demo/brc-ios-demo/DemoApp.swift
@@ -0,0 +1,19 @@
+//
+//  brc_ios_demoApp.swift
+//  brc-ios-demo
+//
+//  Created by Brad Patras on 4/24/22.
+//
+
+import BasicRemoteConfigs
+import SwiftUI
+
+@main
+struct DemoApp: App {
+	let brc = BasicRemoteConfigs(remoteURL: URL(string: "https://github.com/BradPatras/basic-remote-configs/raw/main/examples/simple.json")!)
+    var body: some Scene {
+        WindowGroup {
+            ContentView(brc: brc)
+        }
+    }
+}
